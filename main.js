f77721ae4decdad23a77151189aabca965c06470
larry-kraus <krauslarry@gmail.com>
2016-11-16 16:37:52 -0700

    all 7 columns work

--- main.js
+++ main.js
@@ -34,8 +34,194 @@ var placeChipColumn1 = function() {
 	}
 }
 
+var placeChipColumn2 = function() {
+	var status1 = (r1c2.getAttribute('class') === "chip");
+	var status2 = (r2c2.getAttribute('class') === "chip");
+	var status3 = (r3c2.getAttribute('class') === "chip");
+	var status4 = (r4c2.getAttribute('class') === "chip");
+	var status5 = (r5c2.getAttribute('class') === "chip");
+	var status6 = (r6c2.getAttribute('class') === "chip");
+
+	if (status5) {
+		r6c2.setAttribute('class', 'chip');
+	}
+	else if (status4) {
+		r5c2.setAttribute('class', 'chip');
+	}
+	else if (status3) {
+		r4c2.setAttribute('class', 'chip');
+	}
+	else if (status2) {
+		r3c2.setAttribute('class', 'chip');
+	}
+	else if (status1) {
+		r2c2.setAttribute('class', 'chip');
+	}
+	else {
+		r1c2.setAttribute('class', 'chip');
+	}
+}
+
+var placeChipColumn3 = function() {
+	var status1 = (r1c3.getAttribute('class') === "chip");
+	var status2 = (r2c3.getAttribute('class') === "chip");
+	var status3 = (r3c3.getAttribute('class') === "chip");
+	var status4 = (r4c3.getAttribute('class') === "chip");
+	var status5 = (r5c3.getAttribute('class') === "chip");
+	var status6 = (r6c3.getAttribute('class') === "chip");
+
+	if (status5) {
+		r6c3.setAttribute('class', 'chip');
+	}
+	else if (status4) {
+		r5c3.setAttribute('class', 'chip');
+	}
+	else if (status3) {
+		r4c3.setAttribute('class', 'chip');
+	}
+	else if (status2) {
+		r3c3.setAttribute('class', 'chip');
+	}
+	else if (status1) {
+		r2c3.setAttribute('class', 'chip');
+	}
+	else {
+		r1c3.setAttribute('class', 'chip');
+	}
+}
+
+var placeChipColumn4 = function() {
+	var status1 = (r1c4.getAttribute('class') === "chip");
+	var status2 = (r2c4.getAttribute('class') === "chip");
+	var status3 = (r3c4.getAttribute('class') === "chip");
+	var status4 = (r4c4.getAttribute('class') === "chip");
+	var status5 = (r5c4.getAttribute('class') === "chip");
+	var status6 = (r6c4.getAttribute('class') === "chip");
+
+	if (status5) {
+		r6c4.setAttribute('class', 'chip');
+	}
+	else if (status4) {
+		r5c4.setAttribute('class', 'chip');
+	}
+	else if (status3) {
+		r4c4.setAttribute('class', 'chip');
+	}
+	else if (status2) {
+		r3c4.setAttribute('class', 'chip');
+	}
+	else if (status1) {
+		r2c4.setAttribute('class', 'chip');
+	}
+	else {
+		r1c4.setAttribute('class', 'chip');
+	}
+}
+
+var placeChipColumn5 = function() {
+	var status1 = (r1c5.getAttribute('class') === "chip");
+	var status2 = (r2c5.getAttribute('class') === "chip");
+	var status3 = (r3c5.getAttribute('class') === "chip");
+	var status4 = (r4c5.getAttribute('class') === "chip");
+	var status5 = (r5c5.getAttribute('class') === "chip");
+	var status6 = (r6c5.getAttribute('class') === "chip");
+
+	if (status5) {
+		r6c5.setAttribute('class', 'chip');
+	}
+	else if (status4) {
+		r5c5.setAttribute('class', 'chip');
+	}
+	else if (status3) {
+		r4c5.setAttribute('class', 'chip');
+	}
+	else if (status2) {
+		r3c5.setAttribute('class', 'chip');
+	}
+	else if (status1) {
+		r2c5.setAttribute('class', 'chip');
+	}
+	else {
+		r1c5.setAttribute('class', 'chip');
+	}
+}
+
+var placeChipColumn6 = function() {
+	var status1 = (r1c6.getAttribute('class') === "chip");
+	var status2 = (r2c6.getAttribute('class') === "chip");
+	var status3 = (r3c6.getAttribute('class') === "chip");
+	var status4 = (r4c6.getAttribute('class') === "chip");
+	var status5 = (r5c6.getAttribute('class') === "chip");
+	var status6 = (r6c6.getAttribute('class') === "chip");
+
+	if (status5) {
+		r6c6.setAttribute('class', 'chip');
+	}
+	else if (status4) {
+		r5c6.setAttribute('class', 'chip');
+	}
+	else if (status3) {
+		r4c6.setAttribute('class', 'chip');
+	}
+	else if (status2) {
+		r3c6.setAttribute('class', 'chip');
+	}
+	else if (status1) {
+		r2c6.setAttribute('class', 'chip');
+	}
+	else {
+		r1c6.setAttribute('class', 'chip');
+	}
+}
+
+var placeChipColumn7 = function() {
+	var status1 = (r1c7.getAttribute('class') === "chip");
+	var status2 = (r2c7.getAttribute('class') === "chip");
+	var status3 = (r3c7.getAttribute('class') === "chip");
+	var status4 = (r4c7.getAttribute('class') === "chip");
+	var status5 = (r5c7.getAttribute('class') === "chip");
+	var status6 = (r6c7.getAttribute('class') === "chip");
+
+	if (status5) {
+		r6c7.setAttribute('class', 'chip');
+	}
+	else if (status4) {
+		r5c7.setAttribute('class', 'chip');
+	}
+	else if (status3) {
+		r4c7.setAttribute('class', 'chip');
+	}
+	else if (status2) {
+		r3c7.setAttribute('class', 'chip');
+	}
+	else if (status1) {
+		r2c7.setAttribute('class', 'chip');
+	}
+	else {
+		r1c7.setAttribute('class', 'chip');
+	}
+}
+
 var button1 = document.getElementById('placeButton1');
 button1.addEventListener('click', placeChipColumn1);
+
+var button2 = document.getElementById('placeButton2');
+button2.addEventListener('click', placeChipColumn2);
+
+var button3 = document.getElementById('placeButton3');
+button3.addEventListener('click', placeChipColumn3);
+
+var button4 = document.getElementById('placeButton4');
+button4.addEventListener('click', placeChipColumn4);
+
+var button5 = document.getElementById('placeButton5');
+button5.addEventListener('click', placeChipColumn5);
+
+var button6 = document.getElementById('placeButton6');
+button6.addEventListener('click', placeChipColumn6);
+
+var button7 = document.getElementById('placeButton7');
+button7.addEventListener('click', placeChipColumn7);
 
 
 console.log("aww yeahh");
