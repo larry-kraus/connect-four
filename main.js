94eb2f34af637ef8901c280b07e90e8248eddb80
larry-kraus <krauslarry@gmail.com>
2016-11-16 12:55:43 -0700

    place chip button works for r2c1

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 var r1c2 = document.getElementById('r1c2');
+var r2c1 = document.getElementById('r2c1');
 
 var r1c1status;
 var statusr1c1;
@@ -10,19 +11,35 @@ var placeChipColumn1 = function() {
 
 	console.log(r1c1.getAttribute('class'));
 
-	//return r1c1 = "occupied";
+	return r1c1 = "occupied";
+}
+
+var r1c1 = document.getElementById('r1c1');
+	r1c1.setAttribute('class', 'chip');
+var placeChipColumn1Part2 = function() {
+
+	if (r1c1.getAttribute('class') === "chip") {
+		console.log('yes');
+
+		r2c1.setAttribute('class', 'chip');
+	}
+	else {
+		console.log('no');
+	}
+
 }
 
 var button1 = document.getElementById('placeButton1');
-button1.addEventListener('click', placeChipColumn1);
+button1.addEventListener('click', placeChipColumn1Part2);
 
 console.log(r1c1 + "2");
 
 //console.log(placeChipColumn1());
-//var r1c1status = function {
-//	placeChipColumn1();
-//}
-//console.log(r1c1status);
+
+var r1c1status = function() {
+	placeChipColumn1();
+}
+console.log(r1c1status);
 
 
 console.log("all is well...potentially");
