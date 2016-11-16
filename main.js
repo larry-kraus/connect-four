16ba63c538cb22cf1dd02c376f9e9d9ba7e23a6a
larry-kraus <krauslarry@gmail.com>
2016-11-16 12:21:37 -0700

    place chip button works for row 1

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("JS linked");
+
+var r1c2 = document.getElementById('r1c2');
+
+var r1c1status;
+var statusr1c1;
+
+var placeChipColumn1 = function() {
+	var r1c1 = document.getElementById('r1c1');
+	r1c1.setAttribute('class', 'chip');
+
+	console.log(r1c1.getAttribute('class'));
+
+	//return r1c1 = "occupied";
+}
+
+var button1 = document.getElementById('placeButton1');
+button1.addEventListener('click', placeChipColumn1);
+
+console.log(r1c1 + "2");
+
+//console.log(placeChipColumn1());
+//var r1c1status = function {
+//	placeChipColumn1();
+//}
+//console.log(r1c1status);
+
+
+console.log("all is well...potentially");
